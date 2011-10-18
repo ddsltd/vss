ac3a05b74ffed626e14dabee1725d39801fc0ad3
Mahesh Dsouza <mahesh.dsouza@gmail.com>
2011-10-17 22:48:54 -0400

    Done with static pages

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,14 +1,18 @@
 class PagesController < ApplicationController
   def about
+    @title = "About Page"
   end
 
   def help
+    @title ="Home Page"
   end
 
   def contactus
+    @title = "Contact Us"
   end
 
   def copyright
+    @title = "Copyright"
   end
 
 end
